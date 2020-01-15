d5b5cb8a5743496267d7462abcc349cd6e68f540
Jean-Maxime ALEXANDRE <jeanmaxime.alexandre@ynov.com>
2020-01-15 15:50:32 +0100

    oki

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+            <img src=".\assets\invit.png" width="30px" height="25px" >
+            <router-link to="/">jackApi v1.9.18.4</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+* {
+  margin: 0%;
+  padding: 0%;
+  
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Press Start 2P', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #FfF;
+  
 }
 
 #nav {
-  padding: 30px;
+  height: 30PX;
+  background-color: #696969;
 }
 
 #nav a {
@@ -26,7 +34,17 @@
   color: #2c3e50;
 }
 
+img {
+  position: absolute;
+  top: 2.5px;
+  left: 5px;
+}
+
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color:  #000000;
+  position: absolute;
+  top: 5px;
+  left: 40PX;
+  text-decoration: none;
 }
 </style>
---
+++ src/views/Joke.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="fond" style="padding-top: 50px">
+      <span> C:\User: cd Jack </span><br><br>
+      <span> C:\User\Jack: run JackApi.exe </span><br><br>
+      <span> C:\User\Jack: server JackApi v1.9.18.4 running on http://localhost:8080/</span>
+        <div v-for="element in results" :key="element.id" >
+            <br>
+            <br>
+            <span class="boldRed">C:\User\Jack\Id:  </span><span class="text">{{ element.id }}</span>
+            <br>
+            <br>
+            <span class="boldRed"> C:\User\Jack\Type:  </span><span class="text">{{ element.type }}</span>
+            <br>
+            <br>
+            <span class="boldRed"> C:\User\Jack\Joke:  </span><span class="text">{{ element.setup }}</span>
+            <br>
+            <br>
+            <span class="boldRed"> C:\User\Jack\Punchline:  </span><span class="text">{{ element.punchline }}</span>
+            <br>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        name: "Joke",
+        data(){
+            return{
+                results: []
+            }
+        },
+        methods: {
+            getMyJoke(){
+                axios.get('https://official-joke-api.appspot.com/jokes/programming/ten')
+                .then((joke) => {
+                    console.log(joke.request.response);
+                    this.results = JSON.parse(joke.request.response)
+                })
+                .catch(() => {
+                    this.results = "PAS DE JOKE JE SUIS LA TRISTESSE";
+                })
+            }
+        },
+        mounted: function () {
+            this.getMyJoke()
+        }
+    }
+</script>
+
+<style>
+    .fond{
+        background-color: #000000 ;
+        background-repeat: no-repeat;
+        text-align: left;
+        
+        
+    }
+</style>
